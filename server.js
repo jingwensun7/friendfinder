52a53f6de1ae39eeabf977c6c5980f22f84ce418
jingwensun7 <nataly.jingwen.sun@gmail.com>
2019-06-14 03:39:51 -0700

    html, html routes

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Add the application routes
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
