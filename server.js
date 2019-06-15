fd850d9a0e7f99de76f0f9e67503aab01592952a
jingwensun7 <nataly.jingwen.sun@gmail.com>
2019-06-15 11:04:23 -0700

    heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var path = require("path");
+require("dotenv").config();
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
